a8496432b32ecf94b94c3ec41356d397e851dc27
Stephanus van Zyl <beurar@gmail.com>
2025-06-10 18:18:44 +0200

    Update mod metadata

--- meta.cpp
+++ meta.cpp
@@ -1,4 +1,4 @@
 protocol = 1;
-publishedid = 3477315778;
-name = "Elephantasian Health Regen and Incap System";
+publishedid = 3496635606;
+name = "Viceroy's STALKER ALife";
 timestamp = 5250522511134390247;
