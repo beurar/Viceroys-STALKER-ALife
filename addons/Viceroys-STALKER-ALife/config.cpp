c640cea384e87e72e8d3795359d82449ff77b99e
Stephanus van Zyl <beurar@gmail.com>
2025-06-16 19:42:35 +0200

    Update config.cpp

--- addons/Viceroys-STALKER-ALife/config.cpp
+++ addons/Viceroys-STALKER-ALife/config.cpp
@@ -272,6 +272,6 @@ class Extended_PreInit_EventHandlers
 {
     class VIC_StalkerALife_PreInit
     {
-        init = "call compile preprocessFileLineNumbers '\Viceroys-STALKER-ALife\functions\core\fn_masterInit.sqf'";
+        init = "call compile preprocessFileLineNumbers 'Viceroys-STALKER-ALife\functions\core\fn_masterInit.sqf'";
     };
 };
